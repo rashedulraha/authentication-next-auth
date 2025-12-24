e3e029fe12ba7be065ede185dcb4e4b84a58c602
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-12-24 15:31:29 +0600

    impliment  2 route and 2 page and adding more content

---
+++ src/app/private/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Private = () => {
+  return (
+    <div>
+      <h1 className="text-5xl  text-center font-bold mt-10">Private Route</h1>
+    </div>
+  );
+};
+
+export default Private;
---
+++ src/app/public/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Public = () => {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold text-center mt-10">Public route</h1>
+    </div>
+  );
+};
+
+export default Public;
