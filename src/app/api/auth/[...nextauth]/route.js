a803cd0eafda326c2e80858686d620ae0bad0119
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-12-24 15:01:14 +0600

    adding callback and and validation user data to and set token

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,37 +1,42 @@
+import { dbConnect } from "@/lib/dbConnect";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
 
-const userList = [
-  { name: "rashedul", password: "1234" },
-  { name: "islam", password: "5678" },
-  { name: "abdullah", password: "9101" },
-];
+// const userList = [
+//   { name: "rashedul", password: "1234" },
+//   { name: "islam", password: "5678" },
+//   { name: "abdullah", password: "9101" },
+// ];
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     CredentialsProvider({
       // Sign in with name button
-      name: "Sign in with email and password",
+      name: "Email and password",
 
       //  form inputs
 
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
-        Secret: {
-          label: "Secret",
-          type: "number",
-          placeholder: "Enter your secret code",
+        email: { label: "Email", type: "email", placeholder: "Enter email" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "enter your valid password",
         },
       },
-      async authorize(credentials, req) {
-        const { username, password } = credentials;
-        //  my own login logic
-        const user = userList?.find((u) => u.name == username);
-        if (!user) return null;
 
-        const isPasswordOk = user.password == password;
+      async authorize(credentials, req) {
+        const { email, password } = credentials;
+        //  my own login logic
+        // const user = userList?.find((u) => u.name == username);
+
+        const user = await dbConnect("users").findOne({ email });
+        if (!user) return null;
+        console.log("user data:", user);
+
+        const isPasswordOk = await bcrypt.compare(password, user.password);
         if (isPasswordOk) return user;
 
         return null;
@@ -39,6 +44,28 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
+    async session({ session, token, user }) {
+      if (token) {
+        session.role = token.role;
+      }
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      if (user) {
+        token.email = user.email;
+        token.role = user.role;
+      }
+      return token;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
