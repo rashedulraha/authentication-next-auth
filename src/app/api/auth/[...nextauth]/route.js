43c7a657a767913838e6f816e3718f4e1dec7065
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-12-24 18:08:09 +0600

    basic learning complete

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import { dbConnect } from "@/lib/dbConnect";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 import bcrypt from "bcryptjs";
 
 // const userList = [
@@ -43,6 +44,10 @@ export const authOptions = {
       },
     }),
     // ...add more providers here
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
   ],
 
   callbacks: {
